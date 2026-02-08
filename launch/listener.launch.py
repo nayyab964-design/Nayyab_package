93920e76d2d37c0d48ea1ddc98ba0a6b29dcaa41
nayyab964-design <nayyab964@gmail.com>
2026-02-09 01:44:30 +0500

    Update listener.launch.py

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -1,9 +1,9 @@
 from launch import LaunchDescription
-from launch_ros.actions import node
+from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='demo_noodes_py',
+            package='demo_nodes_py',
             executable='listener'
                 )
     ])
